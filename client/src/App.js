2a96f139e51cd107b1b8d6144db5a001219b5f1f
kmcgee92 <kaseycmcgee@gmail.com>
2021-12-23 12:34:23 -0700

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./millsPlace.jpeg";
+import hairdresser from "./hairdresser.jpeg";
+import "./App.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        style={{
+          fontWeight: "900",
+          fontFamily: "cursive",
+          fontSize: "80px",
+        }}
+      >
+        Millies Place
       </header>
+      <h1>Thank Millie you are the best!</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ width: "80vw", paddingTop: "100px" }}>
+          <Carousel autplay infiniteLoop transitionTime={500}>
+            <div>
+              <img src={logo} alt="asdasd" />
+            </div>
+          </Carousel>
+        </div>
+      </div>
+      <div className="App-header"></div>
     </div>
   );
 }
